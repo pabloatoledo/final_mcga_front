df4421a85790d823606e67710cb6d2590f46129f
pabloatoledo <103152688+pabloatoledo@users.noreply.github.com>
2023-07-31 01:11:24 -0300

    MCGA-FRONT-010: levanta datos de mongoose :D

--- src/redux/Productos/prod.actions.js
+++ src/redux/Productos/prod.actions.js
@@ -1,5 +1,5 @@
 import {ADD_PROD, EDIT_PROD, REM_PROD, GET_PROD_CLOUD_SUCCESS, GET_PROD_CLOUD_FAIL} from './prod.types';
-
+import {useState} from 'react'
 
 //const dispatch = useDispatch()
 
@@ -30,20 +30,40 @@ export const getProdCloud = () => async dispatch => {
   //     'Accept': 'application/json'
   //   }
   // }
-  
+
+  const [getDataCloud, setGetDataCloud] = useState(false)
+
   try {
-    const resp = await fetch('https://final-mcga-back.vercel.app/exam_01_mcga/products/all')
-    console.log(resp)
-    if (resp.ok) {
-      dispatch({
-        type: GET_PROD_CLOUD_SUCCESS,
-        payload: resp
-      })
-    } else {
-      dispatch({
-        type: GET_PROD_CLOUD_FAIL,
-      })
-    }
+    await fetch('https://final-mcga-back.vercel.app/exam_01_mcga/products/all')
+    .then(function(respuesta) {
+      if (respuesta.ok) {
+        setGetDataCloud(true)
+      }
+      return respuesta.json()
+    })
+    .then((data) => {
+      if (getDataCloud) {
+        const prodCloud = data.data
+        console.log("entro ok")
+        console.log(prodCloud)
+
+        if (prodCloud.length > 0) {
+          prodCloud.map((producto) => {
+            dispatch({
+              type: ADD_PROD,
+              payload: producto
+            })
+          })
+        }
+
+        
+      } else {
+        dispatch({
+          type: GET_PROD_CLOUD_FAIL,
+        })
+      }
+    })
+    
   }
   catch (error) {
     dispatch({
